cc7836d8282217dedd6923ceeb3cf9a43bf05d88
jcarlson <jake.carlson90@gmail.com>
2023-08-18 19:11:08 -0700

    updates

--- config/checkToken.js
+++ config/checkToken.js
@@ -4,7 +4,7 @@
 
 // If its valid I need to set the logged in user for the req, res cycle
 
-const jwt = require('jsonwebtoken')
+/*
 
 module.exports = (req, res, next) => {
     let token = req.get('Authoriztion')
@@ -19,4 +19,30 @@ module.exports = (req, res, next) => {
         req.user = null
         return next()
     }
+}*/
+const jwt = require('jsonwebtoken')
+module.exports = function (req, res, next) {
+  // Check for the token being sent in a header or a query parameter
+  let token = req.get('Authorization') || req.query.token
+  if (token) {
+    token = token.replace('Bearer ', '')
+    // Check if token is valid and not expired
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+      req.user = err ? null : decoded.user
+      // Can remove this...
+      // If your app doesn't care
+      req.exp = err ? null : new Date(decoded.exp * 1000)
+    })
+    return next()
+  } else {
+    // No token was sent
+    req.user = null
+    return next()
+  }
 }
+
+
+
+
+
+
--- config/ensureLoggedIn.js
+++ config/ensureLoggedIn.js
@@ -1,4 +1,9 @@
-module.exports = (req, res, next) => {
+/*module.exports = (req, res, next) => {
     if (req.user) return next() 
-    res.status('401').json({ msg: 'unauthorized'})
-}
+    res.status(401).json({ msg: 'unauthorized'})
+}*/
+
+module.exports = function (req, res, next) {
+    if (!req.user) return res.status(401).json('Unauthorized')
+    next()
+  }
